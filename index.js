e717a1e89a9c2d3be0380d9a54abb28d6a88d170
rosemanjake <roseman.jake@gmail.com>
2022-02-06 17:24:16 +0000

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const HOST = "http://localhost:";
+const HOST = "localhost";
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
