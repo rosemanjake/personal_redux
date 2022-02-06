09d1000d019345c48e7648001a00b051070ff8ce
rosemanjake <roseman.jake@gmail.com>
2022-02-06 17:16:31 +0000

    test

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(8080, function () {
+    console.log('Express started on http://localhost:' + 8080 + '; press Ctrl-C to terminate.');
+});
