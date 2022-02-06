a78483fc5a32d5673a495c9a14ae2caec314b1a0
rosemanjake <roseman.jake@gmail.com>
2022-02-06 17:34:41 +0000

    log test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+var morgan  = require('morgan')
+
 const app = express();
 const PORT = 8080;
 const HOST = "localhost";
@@ -7,7 +9,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(morgan('combined'))
+
 app.listen(PORT, HOST, function(err) {
     if (err) return console.log(err);
     console.log("Listening at http://%s:%s", HOST, PORT);
   });
+
