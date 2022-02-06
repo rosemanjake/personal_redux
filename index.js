808c231dbd9dad0e03857305f6ff4741af0792f1
rosemanjake <roseman.jake@gmail.com>
2022-02-06 17:17:46 +0000

    test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 80
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(8080, function () {
-    console.log('Express started on http://localhost:' + 8080 + '; press Ctrl-C to terminate.');
+app.listen(port, function () {
+    console.log('Express started on http://localhost:' + port + '; press Ctrl-C to terminate.');
 });
